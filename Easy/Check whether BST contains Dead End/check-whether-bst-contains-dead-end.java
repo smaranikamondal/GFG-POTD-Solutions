22ef2445f5a322f6fc9db6731c9c37ab815a0d92
Smaranika Mondal <smaranikamondal906@gmail.com>
2023-12-01 23:26:34 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Check whether BST contains Dead End/check-whether-bst-contains-dead-end.java
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+
+import java.util.*;
+import java.io.*;
+class Node {
+        int data;
+        Node left, right;
+ 
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+    }
+class BinarySearchTree {
+    public static Node root;
+    BinarySearchTree() { 
+        root = null; 
+    }
+    void insert(int key) {
+       root = insertRec(root, key);
+    }
+    Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+        if (key < root.data)
+            root.left = insertRec(root.left, key);
+        else if (key > root.data)
+            root.right = insertRec(root.right, key);
+        return root;
+    }
+ 
+    public static void inorder()  {
+       inorderRec(root);
+    }
+    public static void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data);
+            inorderRec(root.right);
+        }
+    }
+ 
+    // Driver Program to test above functions
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int []a=new int[n];
+            BinarySearchTree obj1=new BinarySearchTree();
+            for(int i=0;i<n;i++)
+            {
+                int b=sc.nextInt();
+                obj1.insert(b);
+            }
+            //inorder();
+            Solution obj=new Solution();
+            boolean k=obj.isDeadEnd(root);
+            if(k==true)
+            System.out.println("1");
+            else
+            System.out.println("0");
+        }       
+    }
+}
+
+
+// } Driver Code Ends
+
+
+/*class Node {
+        int data;
+        Node left, right;
+ 
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+    }*/
+
+//Function should return true if a deadEnd is found in the bst otherwise return false.
+class Solution
+{
+    public static boolean isDeadEnd(Node root)
+    {
+        //Add your code here.
+        HashSet<Integer> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
+        solve(root,set,list);
+        // for(int x: list) System.out.print(x+" ");
+        
+        for(int x : list){
+            if(x==1 && set.contains(x+1)) return true;
+            else if(set.contains(x-1) && set.contains(x+1)) return true;
+        }
+        return false;
+    }
+    
+    static void solve(Node node, HashSet<Integer> set, List<Integer> list){
+        if(node==null) return;
+        if(node.left==null && node.right==null) list.add(node.data);
+        solve(node.left,set,list);
+        set.add(node.data);
+        solve(node.right,set,list);
+    }
+}
