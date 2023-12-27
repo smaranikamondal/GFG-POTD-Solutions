2d228807b7420f902229c0479908d5474a383ee8
Smaranika Mondal <smaranikamondal906@gmail.com>
2023-12-27 19:22:37 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Anti Diagonal Traversal of Matrix/anti-diagonal-traversal-of-matrix.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            
+            String input_line[] = br.readLine().trim().split("\\s+");
+            int[][] matrix = new int[n][n];
+            for(int i=0; i<n; i++) {
+                for (int j = 0; j < n; ++j) {
+                    matrix[i][j] = Integer.parseInt(input_line[i * n + j]); 
+                }
+            }
+            Solution obj = new Solution();
+            int[] ans = obj.antiDiagonalPattern(matrix);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] antiDiagonalPattern(int[][] matrix)
+    {
+        // Code here
+        int n = matrix.length, k=0, x=0;
+        int arr[] = new int[n*n];
+        
+        for(int i=0; i<n; i++){
+            k=0;
+            for(int j=i; j>=0; j--) arr[x++] = matrix[k++][j];
+        }
+        
+        for(int i=1; i<n; i++){
+            k=i;
+            for(int j=n-1; j>=i; j--) arr[x++] = matrix[k++][j];
+        }
+        
+        return arr;
+
+    }
+}
