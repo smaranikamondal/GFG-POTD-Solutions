7ad425bf0a2fbe1b92038619dc577ec0b53c16fc
Smaranika Mondal <smaranikamondal906@gmail.com>
2024-01-13 23:01:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Insertion Sort for Singly Linked List/insertion-sort-for-singly-linked-list.java
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class insertion
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			insertion llist = new insertion(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.insertionSort(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    */
+class Solution
+{
+     static Node help(Node ans,Node temp){
+        if(ans==null || ans.data>=temp.data){
+            temp.next=ans;
+            ans=temp;
+        }
+        else{
+            Node temp2=ans;
+            while(temp2.next!=null && temp2.next.data<temp.data){
+                temp2=temp2.next;
+            }
+            temp.next=temp2.next;
+            temp2.next=temp;
+        }
+        return ans;
+    }
+    public static Node insertionSort(Node head_ref)
+    {
+        //code here
+        Node temp=head_ref;
+        Node ans=null;
+        while(temp!=null){
+            Node nxt = temp.next;
+            ans = help(ans,temp);
+            temp=nxt;
+        }
+        return ans;
+    }
+}
