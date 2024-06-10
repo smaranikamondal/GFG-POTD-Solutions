ce983c6bc1b22e59cde58eae0527ec14b73e4572
Smaranika Mondal <smaranikamondal906@gmail.com>
2024-06-10 23:43:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Nuts and Bolts Problem/nuts-and-bolts-problem.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            char[] nuts = new char[n], bolts = new char[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                nuts[i] = (inputLine[i].charAt(0));
+            }
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                bolts[i] = (inputLine[i].charAt(0));
+            }
+
+            new Solution().matchPairs(n, nuts, bolts);
+            for (int i = 0; i < n; i++) {
+                System.out.print(nuts[i] + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < n; i++) {
+                System.out.print(bolts[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        // code here
+        char temp[]={'!','#','$','%','&','*','?','@','^'};
+        Map<Character,Integer> mm = new HashMap<>();
+        for(int i=0;i<n;i++)mm.put(nuts[i],1);
+        int j=0;
+        for(int i=0;i<9;i++){
+            if(mm.get(temp[i])!=null){
+                nuts[j]=temp[i];
+                bolts[j]=temp[i];
+                j++;
+            }
+        }
+
+    }
+}
