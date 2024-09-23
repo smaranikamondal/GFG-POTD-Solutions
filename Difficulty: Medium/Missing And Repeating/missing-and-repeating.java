e8d7300dce427bf978cf2c54398093d7701529a5
Smaranika Mondal <smaranikamondal906@gmail.com>
2024-09-23 23:29:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Missing And Repeating/missing-and-repeating.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().split(" ");
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int[] ans = new Solve().findTwoElement(a);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solve {
+    int[] findTwoElement(int arr[]) {
+        // code here
+        int n=arr.length;
+        int missing=-1,repeating=-1;
+        HashSet<Integer>set=new HashSet<>();
+        for(int i=0;i<n;i++)
+        {
+            if(set.contains(arr[i]))
+            {
+                repeating=arr[i];
+            }
+            else
+            {
+                set.add(arr[i]);
+            }
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(!set.contains(i))
+            {
+                missing=i;
+                break;
+            }
+        }
+        int ans[]={repeating,missing};
+        return ans;
+    }
+}
