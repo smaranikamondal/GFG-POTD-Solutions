0a208833664afd7a43aa3bd2783ce75cd8388716
Smaranika Mondal <smaranikamondal906@gmail.com>
2024-10-21 19:46:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split the Array/split-the-array.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    
+    static final int MOD = 1000000007;
+
+    public static int countgroup(int arr[]) {
+        // Complete the function
+        int n = arr.length;
+        int xorAll = 0;
+        for (int num : arr) {
+            xorAll ^= num;
+        }
+        if (xorAll != 0) {
+            return 0;
+        }
+        long result = 1;
+        for (int i = 0; i < n -1; i++) {
+            result = (result * 2) % MOD;
+        }
+        result = (result - 1 + MOD) % MOD;
+        return (int) result;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            // int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int ans = obj.countgroup(arr);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
