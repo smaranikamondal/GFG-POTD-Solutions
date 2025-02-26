3695041fd1e8e71e4f93be3565b325a31e5ee7d5
Smaranika Mondal <smaranikamondal906@gmail.com>
2025-02-27 03:26:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Get Min from Stack/get-min-from-stack.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Get_Min_From_Stack {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // Number of test cases
+
+        while (T-- > 0) {
+            int q = sc.nextInt(); // Number of queries
+            Solution g = new Solution();
+
+            while (q-- > 0) {
+                int qt = sc.nextInt();
+
+                if (qt == 1) {
+                    int att = sc.nextInt();
+                    g.push(att);
+                } else if (qt == 2) {
+                    g.pop(); // Just call pop(), do not print anything
+                } else if (qt == 3) {
+                    System.out.print(g.peek() + " "); // Print top element
+                } else if (qt == 4) {
+                    System.out.print(g.getMin() + " "); // Print minimum element
+                }
+            }
+            System.out.println("\n~");
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public Solution() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            int poppedElement = stack.pop();
+            if (!minStack.isEmpty() && poppedElement == minStack.peek()) {
+                minStack.pop();
+            }
+        }
+    }
+
+    public int peek() {
+        if (stack.isEmpty()) {
+            return -1;
+        }
+        return stack.peek();
+    }
+
+    public int getMin() {
+        if (minStack.isEmpty()) {
+            return -1;
+        }
+        return minStack.peek();
+    }
+}
